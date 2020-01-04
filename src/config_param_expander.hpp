7c14ff1eb71a38c48ed36aaac13f6ba6d27fb519
salvage <salvage@o2.pl>
2020-01-04 17:26:41 +0100

    Apply pragma once consistently

--- src/config_param_expander.hpp
+++ src/config_param_expander.hpp
@@ -3,6 +3,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#pragma once
+
 //local
 #include <config_node.hpp>
 #include <separator.hpp>
